f2769b85794c0e73f5a19dad1baf0a8e66ec5cf6
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-04-04 14:44:41 +0300

    added soln for pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,9 +2,15 @@
 """
 Returns a list of lists of integers representing the Pascals triangle
 """
-
+from math import factorial
 def pascal_triangle(n):
     """returns pascal's triangle ints"""
     if n <= 0:
         return []
-
+    pascal = []
+    for i in range(n):
+        pascal_temp = []
+        for j in range(i + 1):
+            pascal_temp.append(factorial(i)//(factorial(j)*factorial(i-j)))
+        pascal.append(pascal_temp)
+    return pascal
