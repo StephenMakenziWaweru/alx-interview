2673f9a909c4bc3a7c742a98eb5eb7bed4b95334
Stephen Makenzi Waweru <stevomakenzi@gmail.com>
2022-06-09 08:40:23 +0300

    changed approach

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,39 +1,79 @@
 #!/usr/bin/python3
-"""Island perimeter module"""
+"""
+Define island_perimeter function that finds the perimeter
+of an island in a body of water
+"""
+
+bound_4 = set()
+bound_3 = set()
+bound_2 = set()
+bound_1 = set()
+
+
+def boundary(grid, i, j):
+    """Find cells with either 4, 3, 2 or 1 exposed boundary and add them to
+       appropriate set
+       Args:
+           grid (list): 2d list
+           i (int): row number
+           j (int): column number
+    """
+    boundaries = 0
+    try:
+        if i == 0:
+            boundaries += 1
+        elif grid[i-1][j] == 0:
+            boundaries += 1
+    except:
+        boundaries += 1
+    try:
+        if grid[i+1][j] == 0:
+            boundaries += 1
+    except:
+        boundaries += 1
+    try:
+        if grid[i][j+1] == 0:
+            boundaries += 1
+    except:
+        boundaries += 1
+    try:
+        if j == 0:
+            boundaries += 1
+        elif grid[i][j-1] == 0:
+            boundaries += 1
+    except:
+        boundaries += 1
+
+    if boundaries == 1:
+        bound_1.add((i, j))
+    elif boundaries == 2:
+        bound_2.add((i, j))
+    elif boundaries == 3:
+        bound_3.add((i, j))
+    elif boundaries == 4:
+        bound_4.add((i, j))
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of an island described by grid"""
-    perimeter = 0           # total perimeter of island
-
-    # logic is to check if the cell is surrounded by water or land on either
-    # of top, bottom, left, right sides
-    # if either side is not water, then subtract 1 from cell_perimeter
-    # then add remaining cell perimeter to perimeter - do for all cells
-    for row_i, row in enumerate(grid):
-        for col_i, col in enumerate(row):
-            cell_perimeter = 0
-            if col == 1:
-                cell_perimeter = 4      # perimeter of each cell
-                try:
-                    if grid[row_i - 1][col_i] == 1:  # top
-                        cell_perimeter -= 1
-                except IndexError:
-                    pass
-                try:
-                    if grid[row_i + 1][col_i] == 1:  # bottom
-                        cell_perimeter -= 1
-                except IndexError:
-                    pass
-                try:
-                    if grid[row_i][col_i + 1] == 1:  # right
-                        cell_perimeter -= 1
-                except IndexError:
-                    pass
-                try:
-                    if grid[row_i][col_i - 1] == 1:  # left
-                        cell_perimeter -= 1
-                except IndexError:
-                    pass
-            perimeter += cell_perimeter
+    """
+    Calculate and return perimeter of island in the grid
+    Grid is a rectangular grid where 0s represent water and 1s represent land
+    Each cell is a square with a side length of 1
+    There is only one island
+    Args:
+        grid [list] : 2d list of ints either 0 or 1
+    Return:
+       perimeter of island
+    """
+    if grid == []:
+        return 0
+    l = len(grid)
+    w = len(grid[0])
+    for i in range(l):
+        for j in range(w):
+            if grid[i][j] == 1:
+                boundary(grid, i, j)
+                if len(bound_4) != 0:
+                    return 4
+    perimeter = (len(bound_3) * 3) + (len(bound_2) * 2) + (len(bound_1))
     return perimeter
